58cbcf0d569f23dbcfd167110f9f42075e693123
syr9707 <yrshin9771@gmail.com>
2022-12-30 12:23:39 +0900

    git commit -m "test gitignore"

---
+++ src/main/java/practicejpa/practicejpa01/PracticeJpa01Application.java
@@ -0,0 +1,13 @@
+package practicejpa.practicejpa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJpa01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJpa01Application.class, args);
+	}
+
+}
